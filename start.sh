d196d1ed9179dbb2e95fa9149f49258a37d275b4
LM42 <a1600569@myy.haaga-helia.fi>
2017-04-18 06:12:46 +0000

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,9 @@
 setxkbmap fi
 sudo apt-get update
 sudo apt-get -y install puppet git
-https://github.com/LM42/ninjascript.git
+git clone hhttps://github.com/LM42/ninjascript.git
 cd ninjascript
 bash apply.sh
+
+
 
